720eecce5ea8af1f40bdd446e06ebe69d2d802c0
lje71421@naver.com <lje71421@naver.com>
2023-04-04 23:40:46 +0900

    Upload View

--- harmony/mvc/view/MainView.java
+++ harmony/mvc/view/MainView.java
@@ -2,8 +2,8 @@ package mvc.view;
 
 public class MainView {
     public static void main(String[] args) {
-        System.out.println("[뮤지컬 예매 서비스 이름]");
-        System.out.println("로그인하시겠습니까?");
-        System.out.println("(1) 로그인 정보 입력\t" + "(2) 회원가입");
+        System.out.println("\n================================= [ Harmony ] ====================================");
+        System.out.println("뮤지컬 예매 사이트 하모니에 방문하신 것을 환영합니다.");
+        MenuView.userCheck();
     }
 }
