4ef844feb7896d454ce8f54f0bd5a2e4282ccb35
gyeoul <clsmay.1st@gmail.com>
2023-04-05 09:38:52 +0900

    Update TicketDAO

--- harmony/mvc/dao/TicketDAO.java
+++ harmony/mvc/dao/TicketDAO.java
@@ -7,17 +7,24 @@ import java.util.List;
 public interface TicketDAO {
     /**
      * 예매 등록
+     * String user_id, int musical_id, String seatNum
      * */
-    int ticketInsert(String user_id, int musical_id, String seatNum);
+    int ticketInsert(TicketDTO ticket);
 
     /**
      * 예매 취소
+     * String user_id, int musical_id, String seatNum
      */
-    int ticketDelete(String user_id, int musical_id, String seatNum);
+    int ticketDelete(int ticketID);
 
     /**
      * 예매 내역 조회
      * */
     List<TicketDTO> ticketSelectAll();
+
+    /**
+     * 개별 유저 예매 내역 조회
+     * */
+    List<TicketDTO> ticketSelectById(int ticketID);
 
 }
