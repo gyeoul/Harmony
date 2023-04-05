d3741bbe51b9250fb0966cc726fda93c5971d292
C0012S <alwaysh1002@gmail.com>
2023-04-05 15:15:29 +0900

    Update TicketService & TicketServiceImpl

--- harmony/mvc/service/TicketServiceImpl.java
+++ harmony/mvc/service/TicketServiceImpl.java
@@ -24,11 +24,11 @@ public class TicketServiceImpl implements TicketService{
      * 예매 등록
      * */
     @Override
-    public void ticketInsert(TicketDTO ticket)  {
-        try {
-            SuccessView.messagePrint(ticketDAO.ticketInsert(ticket) > 0 ? "성공" : "실패");
-        } catch (DMLException e){
-            FailView.errorMessage(e.getMessage());
+    public void ticketInsert(TicketDTO ticket) throws DMLException {
+        int result = ticketDAO.ticketInsert(ticket);
+
+        if (result == 0) {
+            throw new DMLException("실패");
         }
     }
 
@@ -36,15 +36,36 @@ public class TicketServiceImpl implements TicketService{
      * 예매 취소
      */
     @Override
-    public void ticketDelete(int ticketID) {
+    public void ticketDelete(int ticketID) throws DMLException {
+        int result = ticketDAO.ticketDelete(ticketID);
 
+        if (result == 0) {
+            throw new DMLException("예매 취소 실패");
+        }
     }
 
     /**
      * 개별 유저 예매 내역 조회
-     * */
+     **/
     @Override
-    public List<TicketDTO> ticketSelectById(int ticketID){
-        return null;
+    public TicketDTO ticketSelectByTicketId(int ticketID){
+        TicketDTO ticketDTO = ticketDAO.ticketSelectByTicketId(ticketID);
+
+        if (ticketDTO == null) {
+            throw new SearchWrongException("예매 번호와 일치하는 예매 내역이 없습니다.");
+        }
+
+        return ticketDTO;
+    }
+
+    @Override
+    public List<TicketDTO> ticketSelectByUserId(String userId) throws SearchWrongException {
+        List<TicketDTO> ticketDTOList = ticketDAO.ticketSelectByUserId(userId);
+
+        if (ticketDTOList.size() == 0) {
+            throw new SearchWrongException("사용자 정보와 일치하는 예매 내역이 없습니다.");
+        }
+
+        return ticketDTOList;
     }
 }
