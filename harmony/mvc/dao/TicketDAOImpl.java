16eb300126fae6bf1f54eae0f6471dc8b419cfbb
C0012S <alwaysh1002@gmail.com>
2023-04-05 10:05:21 +0900

    Update TicketDAOImpl

--- harmony/mvc/dao/TicketDAOImpl.java
+++ harmony/mvc/dao/TicketDAOImpl.java
@@ -25,7 +25,7 @@ public class TicketDAOImpl implements TicketDAO {
      * 예매 등록
      **/
     @Override
-    public int ticketInsert(String user_id, int musical_id, String seatNum){
+    public int ticketInsert(TicketDTO ticket){
         return 0;
     }
 
@@ -33,16 +33,16 @@ public class TicketDAOImpl implements TicketDAO {
      * 예매 취소
      **/
     @Override
-    public int ticketDelete(String user_id, int musical_id, String seatNum){
+    public int ticketDelete(int ticketID){
         Connection con = null;
         PreparedStatement ps = null;
-        String sql = "delete from ticket where musical_id = ?"; // SQL 문 수정 필요
+//        String sql = "delete from ticket where musical_id = ?"; // SQL 문 수정 필요
         int result = 0;
 
         try {
             con = DBManager.getConnection();
-            ps = con.prepareStatement(sql);
-            ps.setInt(1, musical_id);
+//            ps = con.prepareStatement(sql);
+//            ps.setInt(1, musical_id);
 
             result = ps.executeUpdate();
         } catch (SQLException e) {
@@ -59,6 +59,14 @@ public class TicketDAOImpl implements TicketDAO {
      **/
     @Override
     public List<TicketDTO> ticketSelectAll(){
+        return null;
+    }
+
+    /**
+     * 개별 유저 예매 내역 조회
+     **/
+    @Override
+    public List<TicketDTO> ticketSelectById(int ticketID) {
         return null;
     }
 }
