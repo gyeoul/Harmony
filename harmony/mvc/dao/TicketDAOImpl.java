3b980dfd1c48c1b34e3fcfa3785cf85c95084bd6
C0012S <alwaysh1002@gmail.com>
2023-04-05 10:00:07 +0900

    Update TicketDAOImpl - ticketDelete

--- harmony/mvc/dao/TicketDAOImpl.java
+++ harmony/mvc/dao/TicketDAOImpl.java
@@ -1,7 +1,11 @@
 package mvc.dao;
 
+import mvc.common.DBManager;
 import mvc.dto.TicketDTO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class TicketDAOImpl implements TicketDAO {
@@ -9,7 +13,7 @@ public class TicketDAOImpl implements TicketDAO {
 
     /**
      * 외부에서 객체생성 막음
-     */
+     **/
     private TicketDAOImpl() {
     }
 
@@ -19,7 +23,7 @@ public class TicketDAOImpl implements TicketDAO {
 
     /**
      * 예매 등록
-     * */
+     **/
     @Override
     public int ticketInsert(String user_id, int musical_id, String seatNum){
         return 0;
@@ -27,18 +31,34 @@ public class TicketDAOImpl implements TicketDAO {
 
     /**
      * 예매 취소
-     */
+     **/
     @Override
     public int ticketDelete(String user_id, int musical_id, String seatNum){
-        return 0;
+        Connection con = null;
+        PreparedStatement ps = null;
+        String sql = "delete from ticket where musical_id = ?"; // SQL 문 수정 필요
+        int result = 0;
+
+        try {
+            con = DBManager.getConnection();
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, musical_id);
+
+            result = ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBManager.releaseConnection(con, ps);
+        }
+
+        return result;
     }
 
     /**
      * 예매 내역 조회
-     * */
+     **/
     @Override
     public List<TicketDTO> ticketSelectAll(){
         return null;
     }
-
 }
