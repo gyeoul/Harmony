c76f7cb0e73067b083c651b9a28151a6bc62f628
gyeoul <clsmay.1st@gmail.com>
2023-04-06 17:48:11 +0900

    Update Docs

--- harmony/mvc/service/MusicalServiceImpl.java
+++ harmony/mvc/service/MusicalServiceImpl.java
@@ -30,8 +30,8 @@ public class MusicalServiceImpl implements MusicalService {
     }
 
     /**
-     * 작성자 : 전상희
      * 뮤지컬 차트 조회 - 현재 예매 가능한 뮤지컬 목록
+     * 작성자 : 전상희
      */
     @Override
     public List<String> musicalSelectAll() throws SearchWrongException {
@@ -44,8 +44,8 @@ public class MusicalServiceImpl implements MusicalService {
     }
 
     /**
-     * 작성자 : 박창현
      * 뮤지컬의 상세 정보 조회 - 모든 뮤지컬 리스트 반환
+     * 작성자 : 박창현
      * */
     @Override
     public List<MusicalDTO> musicalSelectByTitle(String title) throws SearchWrongException {
@@ -58,8 +58,8 @@ public class MusicalServiceImpl implements MusicalService {
     }
 
     /**
-     * 작성자 : 전상희
      * 뮤지컬의 상세 정보 조회 - 한 개의 뮤지컬의 정보 반환
+     * 작성자 : 전상희
      */
     @Override
     public MusicalDTO musicalDetailSelect(String title) throws SearchWrongException {
@@ -72,8 +72,8 @@ public class MusicalServiceImpl implements MusicalService {
     }
 
     /**
-     * 작성자 : 전상희
      * 좌석 현황 조회 - 선택한 뮤지컬의 현재 좌석 정보 조회
+     * 작성자 : 전상희
      */
     @Override
     public List<SeatDTO> musicalSeatSelect(int musical_id) throws SearchWrongException {
